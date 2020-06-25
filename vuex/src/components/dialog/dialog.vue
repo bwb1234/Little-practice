717edf5de4b4a23270185aee39ded4329d8d1269
baiwenbing <baiwenbing@qq.com>
2020-06-25 12:27:04 +0800

    teach city444444zhaasd

--- vuex/src/components/dialog/dialog.vue
+++ vuex/src/components/dialog/dialog.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         this is dialog vue
+      <p></p>
     </div>
-
 </template>
 <script>
 	export default {
-	    name: "Dialog1112张三"
+	    name: "Dialog1112张三133331111"
 	}
 </script>
 <style>
